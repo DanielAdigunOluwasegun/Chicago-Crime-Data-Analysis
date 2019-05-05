7ad0921cce0315e0210d89e29d4b2be50ab5862d
Daniel <oadigun@clemson.edu>
2019-05-04 22:27:01 -0400

    decision tree model analysis

---
+++ crimes_decisiontree.py
@@ -0,0 +1,32 @@
+import pandas as pd 
+from sklearn.model_selection import train_test_split
+from sklearn import tree 
+from sklearn.metrics import accuracy_score, precision_score, recall_score
+import numpy as np
+from sklearn.metrics import confusion_matrix
+from sklearn import metrics
+dataset = pd.read_csv("data_sets_crime/working_data.csv", encoding='utf-8')
+dataset = dataset.drop(dataset.columns[[2]], axis=1)
+X = dataset.iloc[:,3:8]
+Y = dataset['Ward']
+
+X_train, X_test, Y_train, Y_test = train_test_split(X, Y, test_size=0.25, random_state=1)
+
+
+decision_tree_machine = tree.DecisionTreeClassifier(criterion="gini")
+decision_tree_machine.fit(X_train, Y_train)
+
+y_pred = decision_tree_machine.predict(X_test)
+print(y_pred)
+
+print('r2')
+print(metrics.r2_score(Y_test, y_pred))
+print('mse')
+print(np.sqrt(metrics.mean_squared_error(Y_test, y_pred)))
+
+accuracy_score(Y_test, y_pred)
+print('accuracy')
+print(accuracy_score(Y_test, y_pred))
+print('confusion')
+print(confusion_matrix(Y_test, y_pred))
+
--- crimes_linearreg.py
+++ crimes_linearreg.py
@@ -4,17 +4,16 @@ import pandas as pd
 import numpy as np
 from sklearn.model_selection import train_test_split
 from sklearn import linear_model
-from matplotlib import pyplot as plt
+from sklearn.model_selection import cross_val_score
+from sklearn.datasets import make_classification
 from sklearn import metrics
 
 
 dataset = pd.read_csv("data_sets_crime/working_data.csv", encoding='utf-8')
 print(dataset.head())
-
 dataset = dataset.drop(dataset.columns[[2]], axis=1)
 X = dataset.drop('Ward', axis=1)
 Y = dataset['Ward']
-
 
 X_train, X_test, Y_train, Y_test = train_test_split(X, Y, test_size=0.25, random_state=1)
 
@@ -22,18 +21,15 @@ linreg = linear_model.LinearRegression()
 linreg.fit(X_train, Y_train)
 print(linreg.coef_)
 
-
-
 y_pred = linreg.predict(X_test)
 print(y_pred)
-
-plt.scatter(Y_test, y_pred)
-plt.xlabel('Y_test')
-plt.ylabel('y_pred')
-
-plt.savefig('scatterplot.png')
 
 metrics.r2_score(Y_test, y_pred)
 print(metrics.r2_score(Y_test, y_pred))
 print(np.sqrt(metrics.mean_squared_error(Y_test, y_pred)))
-
+accuracy_score(Y_test, y_pred)
+print(accuracy_score(Y_test, y_pred))
+# print(cross_val_score(linreg, X_train, Y_train, cv=10,scoring='accuracy'))
+# print(cross_val_score(linreg, X_train, Y_train, cv=10,scoring='precision'))
+# print(cross_val_score(linreg, X_train, Y_train, cv=10,scoring='recall'))
+# print(cross_val_score(linreg, X_train, Y_train, cv=10,scoring='f1'))
